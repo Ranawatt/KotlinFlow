9255c3bc234e874244581ae9a318871e136488fe
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-05-27 20:02:56 +0530

    Created new file called SingleNetworkCallViewModel.kt

--- app/src/main/java/com/example/kotlinflow/network/SingleNetworkCallActivity.kt
+++ app/src/main/java/com/example/kotlinflow/network/SingleNetworkCallActivity.kt
@@ -6,8 +6,64 @@ import com.example.kotlinflow.R
 
 class SingleNetworkCallActivity : AppCompatActivity() {
 
+    private lateinit var viewModel: SingleNetworkCallViewModel
+    private lateinit var adapter: ApiUserAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_single_network_call)
+        setupUI()
+        setupViewModel()
+        setupObserver()
+    }
+    private fun setupUI() {
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter =
+            ApiUserAdapter(
+                arrayListOf()
+            )
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                recyclerView.context,
+                (recyclerView.layoutManager as LinearLayoutManager).orientation
+            )
+        )
+        recyclerView.adapter = adapter
+    }
+
+    private fun setupObserver() {
+        viewModel.getUsers().observe(this, Observer {
+            when (it.status) {
+                Status.SUCCESS -> {
+                    progressBar.visibility = View.GONE
+                    it.data?.let { users -> renderList(users) }
+                    recyclerView.visibility = View.VISIBLE
+                }
+                Status.LOADING -> {
+                    progressBar.visibility = View.VISIBLE
+                    recyclerView.visibility = View.GONE
+                }
+                Status.ERROR -> {
+                    //Handle Error
+                    progressBar.visibility = View.GONE
+                    Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        })
+    }
+
+    private fun renderList(users: List<ApiUser>) {
+        adapter.addData(users)
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun setupViewModel() {
+        viewModel = ViewModelProviders.of(
+            this,
+            ViewModelFactory(
+                ApiHelperImpl(RetrofitBuilder.apiService),
+                DatabaseHelperImpl(DatabaseBuilder.getInstance(applicationContext))
+            )
+        ).get(SingleNetworkCallViewModel::class.java)
     }
 }
---
+++ app/src/main/java/com/example/kotlinflow/network/SingleNetworkCallViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.kotlinflow.network
+
+class SingleNetworkCallViewModel(
+    private val apiHelper: ApiHelper,
+    private val dbHelper: DatabaseHelper
+) : ViewModel() {
+
+    private val users = MutableLiveData<Resource<List<ApiUser>>>()
+
+    init {
+        fetchUsers()
+    }
+
+    private fun fetchUsers() {
+        viewModelScope.launch {
+            users.postValue(Resource.loading(null))
+            apiHelper.getUsers()
+                .catch { e ->
+                    users.postValue(Resource.error(e.toString(), null))
+                }
+                .collect {
+                    users.postValue(Resource.success(it))
+                }
+        }
+    }
+
+    fun getUsers(): LiveData<Resource<List<ApiUser>>> {
+        return users
+    }
+
+}
