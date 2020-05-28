1ef5b1d9572a18963cd879425175b8dabb173f75
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-05-28 11:32:02 +0530

    Created new package under base package called completion

---
+++ app/src/main/java/com/example/kotlinflow/base/completion/CompletionActivity.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinflow.base.completion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kotlinflow.R
+
+class CompletionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_completion)
+    }
+}
