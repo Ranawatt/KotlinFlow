1ea71b1fcca78eda3a2e52142fd4387163328903
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-05-31 19:59:09 +0530

    Created FilterActivity under base.filter package to filter the value

---
+++ app/src/main/java/com/example/kotlinflow/base/filter/FilterActivity.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinflow.base.filter
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FilterActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
